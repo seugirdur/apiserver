0ad4c169f929978cc1af1be413808156e0edae2c
Grazi <gamagraziellaferreira@gmail.com>
2022-09-19 09:54:57 -0300

    teste

---
+++ routes/relatorios.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+const { response } = require('express');
+
+router.post('/Relatorios', (req, res, next) => {
+
+    mysql.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({
+                error: error
+            })
+        }
+        
+            const query = `SELECT * FROM Relatorios WHERE idTitulo = ?;`;
+            conn.query(query, [req.body.idTitulo], (error, resultado, fields) => {
+                conn.release();
+
+                if (error) {
+                    return res.status(500).send({
+                        error: error
+                    });
+                }
+
+                if (resultado.length < 1) {
+                    return res.status(200).send({
+                        "error": true,
+                        "message": "Problema nao existe",
+                    });
+                }
+               
+                    if (resultado) {
+
+                         return res.status(200).json(resultado);
+
+                    }
+                    return res.status(200).send({
+                        "error": true,
+                        "message": "Falha na busca de itens",
+
+                    });
+                
+            });
+        
+
+    });
